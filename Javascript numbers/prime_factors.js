75c34493f9a9af99f5830fe952374aab40b3856f
chidalu okechukwu <chifortune567@gmail.com>
2021-07-19 13:24:04 +0100

    Meaning of each block

--- Javascript numbers/prime_factors.js
+++ Javascript numbers/prime_factors.js
@@ -1,4 +1,7 @@
 const primeFactor = (n) => {
+  //we know that 2 divides even numbers so we use 2 to deteroite n
+  //we show the number of time 2 divides n
+  //then pass the remainder to nex block
   if (n % 2 == 0) {
     var count = 0;
     while (n % 2 == 0) {
@@ -8,6 +11,7 @@ const primeFactor = (n) => {
     console.log(2, count);
   }
 
+  //for i greater than square root of n we know we have a prime we pass it to next block
   for (var i = 3; i * i <= n; i++) {
     if (n % i == 0) {
       var count = 0;
@@ -19,6 +23,7 @@ const primeFactor = (n) => {
     }
   }
 
+  //the prime number id printed
   if (n > 2) {
     console.log(n);
   }
